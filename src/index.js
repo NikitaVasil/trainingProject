a5827520640ae21f10953104ca4513602b7784c6
Nikita Vasilnyak <nvasil94@bk.ru>
2024-04-03 23:06:51 +0300

    Произвел первоначальную настройку стилей, добавил header

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './index.html';
-import './styles.scss';
+import './styles/styles.scss';
 
 console.debug('Debug');
+
+//Переменные кнопки и модального окна
+const burgerbutton = document.querySelector('.burger-button');
+const modal = document.querySelector('#mobileOverlay');
+
+burgerbutton.addEventListener('click', ()=> {
+  console.log('Бургер')
+  modal.showModal();
+})
